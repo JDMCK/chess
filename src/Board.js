c9c0a9f57019b343e80034857435a5ff6ad23c7a
Jesse McKenzie <jesse@jessemckenzie.com>
2023-04-20 21:51:27 -0700

    added a working undo feature

--- src/Board.js
+++ src/Board.js
@@ -1,38 +1,64 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Square from './Square';
 import handleShowMoves from "./handlers/handleShowMoves";
 
 const Board = ({ board: BOARD }) => {
 
-  const [board, setBoard] = useState(BOARD);
-  const [isHighlighted, setIsHighlighted] = useState([]);
+  const [boardHistory, setBoardHistory] = useState([BOARD]);
+  const [historyIndex, setHistoryIndex] = useState(0);
+
   const [selectedPiece, setSelectedPiece] = useState({});
+  const [isHighlighted, setIsHighlighted] = useState([]);
   const [isWhiteToMove, setIsWhiteToMove] = useState(true);
 
   const handleMove = (from, to) => {
-    let newBoard = [...board];
+    // Slices future boards (if any)
+    setBoardHistory(history => history.slice(0, historyIndex + 1))
+
+    // Deep copies board
+    let newBoard = JSON.parse(JSON.stringify(boardHistory[historyIndex]));
+
+    // Moves piece
     let piece = newBoard[from.y][from.x];
     newBoard[from.y][from.x] = 'x';
     newBoard[to.y][to.x] = piece;
-    setBoard(newBoard);
+
+    // Adds next board to history
+    setBoardHistory(boardHistory => boardHistory.concat([JSON.parse(JSON.stringify(newBoard))]));
+    setHistoryIndex(previousIndex => previousIndex + 1);
+
+    // Preps next move
     setIsWhiteToMove((previous) => !previous);
     setIsHighlighted([]);
   }
 
+  useEffect(() => {
+    setIsWhiteToMove(historyIndex % 2 === 0);
+  }, [historyIndex])
+
+  const handleUndo = () => {
+    setHistoryIndex(previousIndex => previousIndex > 0 ? previousIndex - 1 : previousIndex);
+  }
+
+  const handleRedo = () => {
+    setHistoryIndex(previousIndex => previousIndex < boardHistory.length - 1 ? previousIndex + 1 : previousIndex);
+  }
+
   return (
     <div id="board">
-      {board.map((row, y) => row.map((piece, x) =>
+      {boardHistory[historyIndex].map((row, y) => row.map((piece, x) =>
         <Square
           key={x + y * 8}
           className={`square r${y + 1} c${x + 1} ${(x + y) % 2 === 0 ? 'dark' : 'light'}`}
           coordinate={{ x, y }}
           piece={piece}
-          handleShowMoves={() => handleShowMoves({ x, y }, board, isWhiteToMove, setIsHighlighted, setSelectedPiece)}
+          handleShowMoves={() => handleShowMoves({ x, y }, boardHistory[historyIndex], isWhiteToMove, setIsHighlighted, setSelectedPiece)}
           handleMove={() => handleMove(selectedPiece, { x, y })}
           isHighlighted={isHighlighted?.some((coordinate) => coordinate.x === x && coordinate.y === y)} />
       ))}
       <p>{isWhiteToMove ? 'White' : 'Black'} to move</p>
-      <button onClick={() => console.log('nope lol')}>UNDO</button>
+      <button onClick={handleUndo}>Back</button>
+      <button onClick={handleRedo}>Forward</button>
     </div>
   );
 }
